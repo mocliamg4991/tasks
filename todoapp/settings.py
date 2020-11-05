0428e4bcc84eeba0f2021bfa639171a445ee2471
mocliamg4991 <clinkz1994@gmail.com>
2020-11-05 17:02:20 +0600

    Update my code

--- todoapp/settings.py
+++ todoapp/settings.py
@@ -17,8 +17,7 @@ import django_heroku
 from django.core.exceptions import ImproperlyConfigured
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
