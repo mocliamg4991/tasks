f3126102a539c719b7f3119cac414ccc260c57e4
mocliamg4991 <clinkz1994@gmail.com>
2020-11-06 14:58:31 +0600

    edit accoutns/views.py

--- accounts/views.py
+++ accounts/views.py
@@ -50,8 +50,7 @@ def edit(request):
 	if request.method == "POST":
 		user_form = UserEditForm(instance=request.user, data=request.POST)
 		profile_form = ProfileEditForm(
-		instance=request.user.profile, data=request.POST,
-		files=request.FILES
+		instance=request.user.profile, data=request.POST
 		)
 		if user_form.is_valid() and profile_form.is_valid():
 			user_form.save()
